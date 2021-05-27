1e1da6d1268aaee137ba043ca1435c5e4f6ab294
Rory Gudka <rdgudka@gmail.com>
2021-05-27 10:53:25 -0400

    Disconnected Google Books, added custom 'database,' and implemented features to control the database from the frontend

--- book-finder/src/Components/Search.js
+++ book-finder/src/Components/Search.js
@@ -1,37 +1,106 @@
 import {useState} from 'react';
 import axios from 'axios';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Result from './Result';
 
 const Search = props => {
-    const [value, setValue] = useState("");
+    const [type, setType] = useState(0);
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
     const [results, setResults] = useState(null);
-    const [changed, setChanged] = useState(true);
 
-    const handleSearch = () => {
+    const handleGet = () => {
         axios.get("http://localhost:3001/book", {
             params: {
-                title:value
+                title:title
             }
         }).then(res => {
-            setChanged(false);
-            setResults(res.data);
+            if(res.data.status === 200) setResults(res.data.data);
+            else alert(res.data.message);
         }).catch(err => {
             console.log("There was an error");
         });
     }
 
+    const handlePost = () => {
+        axios.post("http://localhost:3001/book", undefined, {
+            params: {
+                title:title, 
+                author:author 
+            }
+        }).then(res => {
+            if(res.data.status === 200) setResults(res.data.data);
+            else alert(res.data.message);
+        }).catch(err => {
+            console.log("There was an error");
+        });
+    }
+
+    const handlePut = () => {
+        axios.put("http://localhost:3001/book", undefined, {
+            params: {
+                title:title,
+                author:author
+            }
+        }).then(res => {
+            if(res.data.status === 200) setResults(res.data.data);
+            else alert(res.data.message);
+        }).catch(err => {
+            console.log("There was an error");
+        });
+    }
+
+    const handleDelete = () => {
+        axios.delete("http://localhost:3001/book", {
+            params: {
+                title:title
+            }
+        }).then(res => {
+            if(res.data.status === 200) setResults(null);  
+            else alert(res.data.message);
+        }).catch(err => {
+            console.log("There was an error");
+        });
+    }
+
+    const handleClick = () => {
+        switch(type) {
+            case 0:
+                handleGet();
+                break;
+            case 1:
+                handlePost();
+                break;
+            case 2:
+                handlePut();
+                break;
+            case 3:
+                handleDelete();
+                break;
+            default:
+                console.log("Whacky stuff occurring");
+        }
+    }
+
     return (
         <div>
             <div id="searchWrapper">
-                <TextField label="Book Title" value={value} onChange={e => {
-                    setValue(e.target.value);
-                    setChanged(true);
-                }} />
-                <Button style={{marginLeft:"20px", top:"10px"}} variant="contained" color="primary" onClick={handleSearch}>Search</Button>
+                <div id="selectWrapper">
+                    <Select value={type} onChange={(e, val) => setType(val.props.value)}>
+                        <MenuItem value={0}>Find Author</MenuItem>
+                        <MenuItem value={1}>Create book</MenuItem>
+                        <MenuItem value={2}>Update book</MenuItem>
+                        <MenuItem value={3}>Delete book</MenuItem>
+                    </Select>
+                </div>
+                <TextField label="Title" value={title} onChange={e => setTitle(e.target.value)} />
+                {(type == 1 || type == 2) && <TextField label="Author" value={author} onChange={e => setAuthor(e.target.value)} />}
+                <Button style={{marginLeft:"20px", top:"10px"}} variant="contained" color="primary" onClick={handleClick}>Search</Button>
             </div>
-            {results !== null && !changed && <Result search={value} result={results} />}
+            {results !== undefined && results !== null && <Result search={results.title} result={results.author} />}
         </div>
 
     )
--- express-app/index.js
+++ express-app/index.js
@@ -5,8 +5,77 @@ var app = express()
 
 app.use(cors())
 
-app.get('/book/', cors(), (req, res) => {
+//Whoops if I misspelled anything
+let data = [
+    {
+        name:"The Hunger Games",
+        author:"Suzanne Collins"
+    },
+    {
+        name:"1984",
+        author:"George Orwell"
+    },
+    {
+        name:"The Hobbit",
+        author:"J.R.R. Tolkien"
+    },
+    {
+        name:"Harry Potter",
+        author:"J.K. Rowling"
+    }
+]
+
+const searchByTitle = title => {
+    for(let i = 0; i < data.length; i++) {
+        if(data[i].name.toLowerCase() == title.toLowerCase()) return data[i].author;
+    }
+    return "No results found";
+}
+
+const addBook = (title, author) => {
+    data.push({
+        name:title,
+        author:author
+    });
+}
+
+const findIndex = title => {
+    for(let i = 0; i < data.length; i++) {
+        if(data[i].name.toLowerCase() == title.toLowerCase()) return i;
+    }
+}
+
+const updateBook = (title, author) => {
+    data[findIndex(title)].author = author;
+}
+
+const deleteBook = title => {
+    data.splice(findIndex(title), 1);
+}
+
+app.get('/book/', (req, res) => {
     const title = req.query.title;
+    if(searchByTitle(title) === "No results found") {
+        res.json({
+            status:404,
+            message:"Book not found",
+            data: {
+                title:title
+            }
+        });
+    }
+    else {
+        res.json({
+            status:200,
+            message:"Author successfully found",
+            data: {
+                title:title,
+                author:searchByTitle(title)
+            }
+        });
+    }
+    //Temporarily blocked code to implement custom "database"
+    /*
     axios.get('https://www.googleapis.com/books/v1/volumes', {
         params: {
             q:title
@@ -19,8 +88,123 @@ app.get('/book/', cors(), (req, res) => {
     }).catch(err => {
         res.send("Request failed");
     })
+    */
+});
+
+app.get('/books/', (req, res) => {
+    res.json({
+        status:200,
+        message:"Books successfully found",
+        data: {
+            books:data
+        }
+    });
+})
+
+app.post('/book/', (req, res) => {
+    const title = req.query.title;
+    const author = req.query.author;
+    if(title === undefined || author === undefined) {
+        res.json({
+            status:400,
+            message:"Invalid input",
+            data: {
+                title:title,
+                author:author
+            }
+        });
+    }
+    else if(searchByTitle(title) !== "No results found") {
+        res.json({
+            status:400,
+            message:"Book with title '" + title + "' already exists",
+            data: {
+                title:title,
+                author:author
+            }
+        });
+    }
+    else {
+        addBook(title, author);
+        res.json({
+            status:200,
+            message:"Book successfully added",
+            data: {
+                title:title,
+                author:author
+            }
+        });
+    }
+});
+
+app.put('/book/', (req, res) => {
+    const title = req.query.title;
+    const author = req.query.author;
+    if(title === undefined || author === undefined) {
+        res.json({
+            status:400,
+            message:"Invalid input",
+            data: {
+                title:title,
+                author:author
+            }
+        });
+    }
+    else if(searchByTitle(title) === "No results found") {
+        res.json({
+            status:400,
+            message:"Book with title '" + title + "' doesn't exist",
+            data: {
+                title:title,
+                author:author
+            }
+        });
+    }
+    else {
+        updateBook(title, author);
+        res.json({
+            status:200,
+            message:"Book successfully updated",
+            data: {
+                title:title,
+                author:author
+            }
+        });
+    }
+});
+
+app.delete('/book/', (req, res) => {
+    const title = req.query.title;
+    if(title === undefined) {
+        res.json({
+            status:400,
+            message:"Invalid input",
+            data: {
+                title:title
+            }
+        });
+    }
+    else if(searchByTitle(title) === "No results found") {
+        res.json({
+            status:400,
+            message:"Book with title '" + title + "' doesn't exist",
+            data: {
+                title:title
+            }
+        });
+    }
+    else {
+        deleteBook(title);
+        res.json({
+            status:200,
+            message:"Book successfully deleted",
+            data: {
+                title:title
+            }
+        });
+    }
 })
 
 app.listen(3001, () => {
     console.log("Server started on port 3001")
-})
+});
